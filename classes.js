4c3691a68d96549ade39a842b23bcaecfd2df11f
unknown <dcis.smith@gmail.com$git config --global user.email dcis.smith@gmail.com>
2018-07-03 14:18:06 +1200

    divs 1 and 2 altered

--- classes.js
+++ classes.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
+  two()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -14,11 +15,15 @@ function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
 
-  // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
